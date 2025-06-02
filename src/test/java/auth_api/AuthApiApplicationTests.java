bbbb679489136c882f7db5cda79d823ac2d5721b
mooze <mooze@mooze.com>
2025-06-02 21:39:09 +0200

    test configuration

--- src/test/java/auth_api/AuthApiApplicationTests.java
+++ src/test/java/auth_api/AuthApiApplicationTests.java
@@ -1,15 +1,27 @@
 package auth_api;
 
+import auth_api.controllers.AuthController;
+import auth_api.controllers.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class AuthApiApplicationTests {
 
+	@Autowired
+	private AuthController authController;
+
+	@Autowired
+	private UserController userController;
+
 	@Test
 	void contextLoads() {
+		assertThat(authController).isNotNull();
+		assertThat(userController).isNotNull();
 	}
 
 }
---
+++ src/test/java/auth_api/controllers/AuthControllerTest.java
@@ -0,0 +1,4 @@
+package auth_api.controllers;
+
+public class AuthControllerTest {
+}
