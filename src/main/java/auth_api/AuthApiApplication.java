1612515802314a53fd9d270eae83113c7aff8871
mooze <mooze@mooze.com>
2025-06-02 12:50:55 +0200

    rename

--- src/main/java/auth_api/AuthApiApplication.java
+++ src/main/java/auth_api/AuthApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AUthApiApplication {
+public class AuthApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AUthApiApplication.class, args);
+		SpringApplication.run(AuthApiApplication.class, args);
 	}
 
 }
