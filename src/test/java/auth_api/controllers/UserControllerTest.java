bc8cc1af15acaa0fa7b0c7bc1c13ba4c9d1ed0e5
mooze <mooze@mooze.com>
2025-06-06 11:22:51 +0200

    test user controller

--- src/test/java/auth_api/controllers/UserControllerTest.java
+++ src/test/java/auth_api/controllers/UserControllerTest.java
@@ -1,7 +1,7 @@
 package auth_api.controllers;
 
 import auth_api.database.interfaces.IUserRepository;
-import auth_api.service.IAuthService;
+import auth_api.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
@@ -11,11 +11,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AuthController.class)
+@WebMvcTest(UserController.class)
 @AutoConfigureMockMvc(addFilters = false) //add filters false means auth is bypassed
 public class UserControllerTest {
     @Autowired
@@ -31,6 +34,43 @@ public class UserControllerTest {
     }
 
     @Test
+    public void testThatTheGetMappingCallsTheRepository() throws Exception {
+        mockMvc.perform(get("/api/user/all")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""))
+                .andExpect(status().isOk());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testThatThePostMappingCallsTheRepository() throws Exception {
+
+        final String signinRequest = """
+            {
+              "username": "existingUser",
+              "password": "password123",
+              "email": "asd@asd.dk"
+            }
+            """;
+
+        User expectedUser = new User();
+        expectedUser.setUsername("existingUser");
+        expectedUser.setPassword("password123");
+        expectedUser.setEmail("asd@asd.dk");
+
+        mockMvc.perform(post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(signinRequest))
+                .andExpect(status().isOk());
+
+        verify(userRepository).save(argThat(user ->
+                "existingUser".equals(user.getUsername()) &&
+                        "password123".equals(user.getPassword()) &&
+                        "asd@asd.dk".equals(user.getEmail())
+        ));
+    }
+
+    @Test
     public void testThatItValidatesOnThePostMapping() throws Exception {
         final String signinRequest = """
             {
@@ -40,7 +80,7 @@ public class UserControllerTest {
             }
             """;
 
-        mockMvc.perform(post("/api/user/")
+        mockMvc.perform(post("/api/user")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(signinRequest))
                 .andExpect(status().isBadRequest());
